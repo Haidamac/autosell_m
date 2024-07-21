733787c7feec1722ff1b0a9f6b94349e1beb6092
Haidamac <serge.krinitsa@gmail.com>
2024-07-21 20:40:30 +0300

    delete redudant code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
 // Entry point for the build script in your package.json
+//= require rails-ujs
+//= require bootstrap
+//= require_tree .
+
 import "@hotwired/turbo-rails"
 import "controllers"
 
--- app/controllers/users/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-class Users::SessionsController < Devise::SessionsController
-  def after_sign_in_path_for(resource)
-    if resource.admin?
-      rails_admin_path  # Redirect to Rails Admin dashboard
-    else
-      super
-    end
-  end
-end
